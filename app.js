7b2c5b64b3a18f51e81c7e04a2ed1628b4316496
ak <ak@aks-MacBook-Pro.local>
2021-04-19 19:31:56 -0600

    testing aws codepipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Akhigbe '));
+app.get('/', (req, res) => res.send('I love Utah economy wanna stay here lol'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
